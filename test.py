e8386e4c5dc36e517bf16540a65ed8c99d09b9aa
Kian Wee <46987985+kianweelee@users.noreply.github.com>
2020-09-18 16:16:01 +0800

    Update test.py
    Identify NA values for each column

--- test.py
+++ test.py
@@ -4,3 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 Iris = pd.read_csv("data/Iris.csv")
+
+# Identify the number of NA values for each column
+print(Iris.isnull().sum())
